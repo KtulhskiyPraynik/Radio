2777cd2d25a57f4f6c78391c07fefcc4b1d184b8
KtulhskiyPraynik <96628961+KtulhskiyPraynik@users.noreply.github.com>
2022-05-31 16:46:30 +0700

    Update Radio.java

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -32,43 +32,39 @@ public class Radio {
         return currentVolume;
     }
 
-    public void nextStation(int currentStation) {
+    public void nextStation() {
         if (currentStation < 9) {
             currentStation = currentStation + 1;
         } else {
             currentStation = 0;
         }
-        this.currentStation = currentStation;
+        setCurrentStation(currentStation);
     }
 
-    public void prevStation(int currentStation) {
+    public void prevStation() {
         if (currentStation == 0) {
             currentStation = 9;
         } else {
             currentStation = currentStation - 1;
         }
-        this.currentStation = currentStation;
+        setCurrentStation(currentStation);
     }
 
-    public void increaseVolume(int currentVolume) {
+    public void increaseVolume() {
         if (currentVolume < 10) {
             currentVolume = currentVolume + 1;
         } else {
             currentVolume = 10;
         }
-        this.currentVolume = currentVolume;
+        setCurrentVolume(currentVolume);
     }
 
-    public void reduceVolume(int currentVolume) {
-        if (currentVolume <= 0) {
+    public void reduceVolume() {
+        if (currentVolume == 0) {
             return;
-        }
-        if (currentVolume < 10) {
+        } else {
             currentVolume = currentVolume - 1;
         }
-        if (currentVolume > 10) {
-            return;
-        }
-        this.currentVolume = currentVolume;
+        setCurrentVolume(currentVolume);
     }
 }
